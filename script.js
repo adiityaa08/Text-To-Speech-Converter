09168beff98b49722afe49ce069b9376d23988f5
adiityaa08 <adityasathawane2003@gmail.com>
2024-12-17 13:53:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 let speech = new SpeechSynthesisUtterance();
-
 let voices = [];
-
 let voiceSelect = document.querySelector("select");
 
+// Update voices when available
 window.speechSynthesis.onvoiceschanged = () => {
-   voices = window.speechSynthesis.getVoices();
-   speech.voice = voices[0];
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];  // Default to the first voice
 
-   voices.forEach((voice ,i) => (voiceSelect.options[i] = new Option(voice.name ,i)));
+    // Populate the voice selection dropdown
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
 
-voiceSelect.addEventListener("change", () =>{
+// Change voice when the user selects a new one
+voiceSelect.addEventListener("change", () => {
     speech.voice = voices[voiceSelect.value];
 });
 
-
-
-document.querySelector('button').addEventListener('click', () =>{
+// Play the text when the Play button is clicked
+document.getElementById('playButton').addEventListener('click', () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
-})
+});
+
+// Pause the speech when the Pause button is clicked
+document.getElementById('pauseButton').addEventListener('click', () => {
+    window.speechSynthesis.pause();
+});
+
+// Resume the speech when the Resume button is clicked
+document.getElementById('resumeButton').addEventListener('click', () => {
+    window.speechSynthesis.resume();
+});
+
+// Stop the speech when the Stop button is clicked
+document.getElementById('stopButton').addEventListener('click', () => {
+    window.speechSynthesis.cancel();
+});
